f20756e83b67c9d8f21f56c3bd043926267c7248
quanglinh <19130118@st.hcmuaf.edu.vn>
2024-03-13 11:14:49 +0700

    [2] Competing Consumer

--- consumer.py
+++ consumer.py
@@ -1,7 +1,12 @@
 import pika
-
+import time
+import random
 def on_message_received(ch, method, properties, body):
-    print(f" [x] Received {body}")
+    processing_time = random.randint(1, 6)
+    print(f" [x] {body}: {processing_time}s")
+    time.sleep(processing_time)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    print(f" [x] Done processing")
 
 connection_parameters = pika.ConnectionParameters('localhost')
 
@@ -10,8 +15,8 @@ connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
 
 channel.queue_declare(queue='letterbox')
-
-channel.basic_consume(queue='letterbox', on_message_callback=on_message_received, auto_ack=True)
+# channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='letterbox', on_message_callback=on_message_received)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
--- producer.py
+++ producer.py
@@ -1,5 +1,6 @@
 import pika
-
+import time
+import random
 connection_parameters = pika.ConnectionParameters('localhost')
 
 connection = pika.BlockingConnection(connection_parameters)
@@ -8,10 +9,12 @@ channel = connection.channel()
 
 channel.queue_declare(queue='letterbox')
 
-message = "Hello World!"
-
-channel.basic_publish(exchange='', routing_key='letterbox', body=message)
-
-print(f" [x] Sent {message}")
+messageId = 1
+while True:
+    message = f"message: {messageId}"
+    channel.basic_publish(exchange='', routing_key='letterbox', body=message)
+    print(f" [x] Sent {message}")
+    time.sleep(random.randint(1, 4))
+    messageId += 1
 
 connection.close()
